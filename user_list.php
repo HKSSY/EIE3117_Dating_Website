40497d45a0d4406c2d1d622d56f58a9799982410
HKSSY <94104820+HKSSY@users.noreply.github.com>
2022-06-15 09:41:02 +0800

    Update user_list.php

--- user_list.php
+++ user_list.php
@@ -1,14 +1,14 @@
 <?php
 include('assets/php/config.php');
-// We need to use sessions, so you should always start sessions using the below code.
 session_start();
-// If the user is not logged in redirect to the login page...
+// If the user is not logged in redirect to the login page
 if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.php');
 	exit;
 }
-//Connect to database
+// Connect to database
 include('assets/php/database_connect.php');
+// Display all user information
 $result = mysqli_query($con,"SELECT id, nickname, dob, gender FROM accounts");  
 ?>
 <!DOCTYPE html>
