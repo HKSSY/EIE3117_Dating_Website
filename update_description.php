835a0e3cd27b9260bfc57b717e66d72531572874
HKSSY <94104820+HKSSY@users.noreply.github.com>
2022-06-15 09:42:54 +0800

    Update update_description.php

--- update_description.php
+++ update_description.php
@@ -1,8 +1,7 @@
 <?php
 include('assets/php/config.php');
-// We need to use sessions, so you should always start sessions using the below code.
 session_start();
-// If the user is not logged in redirect to the login page...
+// If the user is not logged in redirect to the login page
 if (!isset($_SESSION['loggedin'])) {
 	header('Location: index.php');
 	exit;
